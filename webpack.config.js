ad78661a8f1dce60bc72e7ef4f00543ac0d14c38
kdenru <kdenru@gmail.com>
2019-11-13 13:19:55 +0700

    deploy to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.jsx',
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js',
+    path: path.join(__dirname, '/build'),
+    filename: 'index.js',
   },
   module: {
     rules: [

7de3c7766d73121aed0abdf7101967c1b300d94b
kdenru <kdenru@gmail.com>
2019-11-13 13:32:42 +0700

    fix production build error

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk'
 
 import { rootReducer, initialState } from './reducer'
 
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : f => f // eslint-disable-line
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line
+    devTools,
   ),
 )
 
